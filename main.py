77951adc309affbb4027793c1fedd1e5a657fc2d
SANTHOSH KRISHNAN R <145760700+santhoshkrishnan30@users.noreply.github.com>
2024-04-25 21:30:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,14 +22,14 @@ def index():
 #    Located at: http://127.0.0.1:8000/AnyNameHere
 @app.get('/Welcome')
 def get_name(name: str):
-    return {'Welcome To Krish Youtube Channel': f'{name}'}
+    return {'Welcome To my project': f'{name}'}
 
 
 
 # 5. Run the API with uvicorn
 #    Will run on http://127.0.0.1:8000
 if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+    uvicorn.run(app, host='0.0.0.0', port=8000)
 #uvicorn main:app --reload
 
 
