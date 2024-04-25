4a3e268c7d628e773390adf1edd626188f5d5aa5
SANTHOSH KRISHNAN R <145760700+santhoshkrishnan30@users.noreply.github.com>
2024-04-25 21:29:13 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -74,4 +74,4 @@ with open("classifier.pkl", "wb") as f:
 # 6. Run the API with uvicorn
 #    Will run on http://127.0.0.1:8000
 if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+    uvicorn.run(app, host='0.0.0.0', port=8000)
